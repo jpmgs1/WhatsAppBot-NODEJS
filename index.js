4954744497cf43ae5803825605c50b9c1de39d8b
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-06-17 19:55:02 -0300

    'v3'

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const qrcode = require('qrcode-terminal');
 const gTTS = require('gtts');
-const fs = require('fs');
 const { Client, MessageMedia } = require('whatsapp-web.js');
-const client = new Client();
+const fs = require('fs');
+
+// Path where the session data will be stored
 const SESSION_FILE_PATH = './session.json';
 
+// Load the session data if it has been previously saved
+let sessionData;
+if(fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
 
+// Use the saved values
+const client = new Client({
+    session: sessionData
+});
+
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
 
 client.on('ready', () => {
     console.log('Client is ready!');
@@ -46,7 +65,7 @@ client.on('message', msg => {
         gtts.save('Voice.mp3', function (err, result){ 
           if(err) { throw new Error(err); } 
 		  	try{
-				client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers.mp3`))
+				client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Voice.mp3`))
 			}
 			catch(c){
 				console.log(c)
@@ -54,7 +73,11 @@ client.on('message', msg => {
 		});
 
 	}
-		
+	
+	if (msg.body == '!rojers'){
+		client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers_image.jpeg`), { sendMediaAsSticker: true }) 
+		client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers.mp3`))
+	}
 
 })
 
