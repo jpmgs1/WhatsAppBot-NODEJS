8664f4db5165860c755b915ab82e7c91eee6e5fe
Rafael de Oliveira Domenegueti <69098216+RafaelDomenegueti@users.noreply.github.com>
2021-07-02 21:31:06 -0300

    V4

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
 const gTTS = require('gtts');
 const { Client, MessageMedia } = require('whatsapp-web.js');
 const fs = require('fs');
+const qrcode = require('qrcode-terminal')
+var request = require("request");
+const axios = require('axios');
 
-// Path where the session data will be stored
 const SESSION_FILE_PATH = './session.json';
+let ws;
+let dataSession;
 
-// Load the session data if it has been previously saved
-let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
+const withSession = () => {
+    dataSession = require(SESSION_FILE_PATH);
+    ws = new Client({ session: dataSession });
+    ws.on('ready', () => console.log('Cliente está pronto!'));
+    ws.on('auth_failure', () => {
+        console.log('** O erro de autenticação regenera o QRCODE (Excluir o arquivo session.json) **');
+    })
+    ws.initialize();
+}
+const withOutSession = () => {
+    ws = new Client();
+    // Geramos o QRCODE no Terminal
+    ws.on('qr', qr => { qrcode.generate(qr, { small: true }); });
+    ws.on('ready', () => console.log('Cliente está pronto!'));
+    ws.on('auth_failure', () => {
+        console.log('** O erro de autenticação regenera o QRCODE (Excluir o arquivo session.json) **');
+    })
+    // Se for autenticado, gera um arquivo com as variáveis  de sessão
+    ws.on('authenticated', (session) => {
+        dataSession = session;
+        fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+            if (err) console.log(err);
+        });
+    });
+    ws.initialize();
 }
 
-// Use the saved values
-const client = new Client({
-    session: sessionData
-});
+(fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withOutSession();
 
-// Save session values to the file upon successful auth
-client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
-});
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', msg => {
+ws.on('message', msg => {
 	if(msg.body === '!ping') {
 		msg.reply('pong');
+	}
+	if (msg.body.startsWith("!r.f ")){
+		let mensagem = msg.body.replace("!r.f ","")
+		request('https://api.simsimi.net/v1/?text='+mensagem+'&lang=pt', function (error, response, body) {
+			let jsonm = JSON.parse(body) 
+			msg.reply(jsonm.success);
+		});	
+
 	}
 	if (msg.body.startsWith("!r.p ")){
         let mensagem = msg.body.replace("!r.p ","")
@@ -42,7 +59,7 @@ client.on('message', msg => {
 		gtts.save('Voice.mp3', function (err, result){ 
 			if(err) { throw new Error(err); }
 			try{
-				client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Voice.mp3`))
+				ws.sendMessage(msg.from, MessageMedia.fromFilePath(`./Voice.mp3`))
 			}
 			catch(c){
 				console.log(c)
@@ -65,7 +82,7 @@ client.on('message', msg => {
         gtts.save('Voice.mp3', function (err, result){ 
           if(err) { throw new Error(err); } 
 		  	try{
-				client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Voice.mp3`))
+				ws.sendMessage(msg.from, MessageMedia.fromFilePath(`./Voice.mp3`))
 			}
 			catch(c){
 				console.log(c)
@@ -73,12 +90,19 @@ client.on('message', msg => {
 		});
 
 	}
-	
+	if (msg.body == "!piu") {
+		axios({
+		method: 'get',
+		url: 'http://teles-jokes.azurewebsites.net/api/GetJoke?code=7skvGJHPnh6jOiZhwNKV0dL0awj9qTorLElJq596sKVHmrmgFJ6u4w==',
+		  })
+			.then(function (response) {
+			  let body = response.data
+			  ws.sendMessage(msg.from, body.joke.replace("||","\n"))
+		});
+	  }
 	if (msg.body == '!rojers'){
-		client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers_image.jpeg`), { sendMediaAsSticker: true }) 
-		client.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers.mp3`))
+		ws.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers_image.jpeg`), { sendMediaAsSticker: true }) 
+		ws.sendMessage(msg.from, MessageMedia.fromFilePath(`./Rojers.mp3`))
 	}
 
 })
-
-client.initialize()
